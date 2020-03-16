e1e3f097cda932663253f99db9237af658b6502c
medcharfi96 <1389@holbertonschool.com>
2020-03-16 22:17:51 +0000

    commit

--- my_printf.c
+++ my_printf.c
@@ -9,18 +9,22 @@
 int _printf(const char *format, ...)
 {
 va_list vl;
-int resultat, sum = 0;
+int resultat = 0, sum = 0, i = 0;
 if (format == NULL)
 resultat = -1;
 return (resultat);
 va_start(vl, format);
 while ((format != NULL) && (format[i] != '\0'))
 {
-if (format[i] != '/0')
+if (format[i] != '%')
 sum++;
-else 
+else if (format[i] == '%' && format[i + 1] != '\0')
+{
+sum++;
 resultat = sum;
-
-return (resultat);
 }
+sum+=2;
+}
+resultat = sum;
+return (resultat);
 }
